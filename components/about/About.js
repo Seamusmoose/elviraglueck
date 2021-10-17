e0bf6a0acc9acef735b80b5ee211dca7631040cd
Seamus O'Sullivan <info@seamuso.com>
2021-10-17 16:47:59 +0200

    about comp

--- components/about/About.js
+++ components/about/About.js
@@ -3,7 +3,14 @@ import styles from './about.module.css'
 export default function About() {
     return (
         <div className={styles.aboutContainer}>
-            Here is about page
+            {/* <div className={styles.image}>
+        <Image src={idImage} alt="profileImage" layout="responsive" />
+      </div>
+      <div className={styles.title}>
+        <h1>Elvira Glück</h1>
+        <h2>Therapie | Supervision | Coaching</h2>
+        <p>Wilkommen! Welcome!</p>
+      </div> */}
         </div>
     )
 };
